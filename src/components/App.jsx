0205b87745defb43163e6dcf3c270b1227ac3f0c
Anas Butt <anasbutt@Anass-MacBook-Pro.local>
2021-11-13 22:35:56 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+    return <div>
+        <Header/>
+        <h1>Hello</h1>
+        <Footer/>
+    </div>
+}
+
+export default App
